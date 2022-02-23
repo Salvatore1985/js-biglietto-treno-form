0a001c957c99f4b7ffc9fafed7c4d32b1b8f44a8
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-23 18:20:25 +0100

    add form

--- js/script.js
+++ js/script.js
@@ -1,54 +1,63 @@
 //*RECUPERO L'ELEMENTO DAL DOM
-
-let elementName = document.getElementById("name");
-let elementLastName = document.getElementById("last-name");
-let elementAge = document.getElementById("age");
-let elementUserYear = document.getElementById("user-year");
-let elementKms = document.getElementById("kms");
+let elementFildName = document.getElementById("fild-name");
+let elementFildLastName = document.getElementById("fild-last-name");
+let elementFildAge = document.getElementById("fild-age");
+let elementFildKms = document.getElementById("fild-kms");
 let elementDiscount = document.getElementById("discount");
 let elementPrice = document.getElementById("price");
 
-//* CHIEDO ALL'UTENTE DI INSERIRE I DATI
-const userName = prompt("Qual'è il tuo Nome ?", "Salvatore");
-console.log("Il tuo Nome è :", userName);
-
-const userLastName = prompt("Qual'è il tuo Cognome ?", "Cascone");
-console.log("Il tuo Cognome è :", userLastName);
-
-const userAge = parseInt(prompt("Quanti anni hai ?", "36"));
-console.log("I tuoi anni sono :", userAge);
-
-const userKms = parseInt(prompt("Quanti KM vuoi percorrere ?", "10"));
-console.log("I Km che hai inserito sono :", userKms);
-
-//* VARIABILE 
-let userDiscount;
-let userPrice = 0.27;
-let sum = userPrice * userKms;
-let currentYear = 2022
-let userYear = currentYear - userAge;
+//*RECUPERO L'EMENTO DAL FORM
+let elementName = document.getElementById("name");
+let elementLastName = document.getElementById("last-name");
+let elementAge = document.getElementById("age");
+let elementKms = document.getElementById("kms");
+let elementButtonGenerate = document.getElementById("generate-ticket");
 
 
-// ! CONVALIDE
-if (userAge < 18) {
-    sum *= 0.83;
-    userDiscount = "Essendo Minorenne hai diritto a uno sconto del 20%";
-} else if (userAge >= 65) {
-    sum *= 0.67;
-    userDiscount = "Essendo Over hai diritto a uno sconto del 60%";
-} else {
-    userDiscount = "Grazie per aver viaggiato con Trenitalia";
 
-};
+//* LEGO UN EVENTO AL CLICK AL BOTTONE "GENERA"
+elementButtonGenerate.addEventListener("click", function () {
+    let nameValue = elementName.value;
+    console.log(nameValue);
+    let lastNameValue = elementLastName.value;
+    console.log(lastNameValue);
+    let ageValue = elementAge.value;
+    console.log(ageValue);
+    let kmsValue = elementKms.value;
+    console.log(kmsValue);
+
+    //* VARIABILE 
+    let userDiscount;
+    let userPrice = 0.27;
+    let sum = userPrice * kmsValue;
+
+
+    // ! CONVALIDE
+    if (ageValue === "min") {
+        sum *= 0.83;
+        userDiscount = "Essendo Minorenne hai diritto a uno sconto del 17%";
+        console.log(sum.toFixed(2));
+    } else if (ageValue === "over") {
+        sum *= 0.67;
+        userDiscount = "Essendo Over hai diritto a uno sconto del 33%";
+        console.log(sum.toFixed(2));
+    } else {
+        userDiscount = "Grazie per aver viaggiato con Trenitalia";
+        console.log(sum.toFixed(2));
+
+    };
+
+});
+
 
 
 
 //* INSERISCO I DATI DEL DOM
 
-elementName.innerHTML = userName + " ;";
-elementLastName.innerHTML = userLastName + " ;";
-elementAge.innerHTML += userAge + " ;";
-elementUserYear.innerHTML = userYear + " ;";
+elementFildName.innerHTML = nameValue + " ;";
+elementFildLastName.innerHTML = userLastName + " ;";
+elementFildAge.innerHTML += userAge + " ;";
+/* elementUserYear.innerHTML = userYear + " ;"; */
 elementKms.innerHTML = userKms + " ;";
 elementDiscount.innerHTML = userDiscount + " ;";
 elementPrice.innerHTML += " Euro " + sum.toFixed(2) + " ;";
